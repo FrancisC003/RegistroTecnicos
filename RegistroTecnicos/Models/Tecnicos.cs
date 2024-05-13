ee5fc1bd357694896bbc519470190ad4d83ae56c
Francis Emanuel Castillo Vasquez <francis_castillo4@ucne.edu.do>
2024-05-13 00:47:11 -0400

    Cree la carpeta "Models" y cree el modelo "Tecnicos"

---
+++ RegistroTecnicos/Models/Tecnicos.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Tecnicos
+{
+    [Key]
+
+    public int TecnicosId { get; set; }
+    [Required(ErrorMessage = "El campo Nombre es obligatorio")]
+    public string? TecnicosName { get; set; }
+    [Range(0.1, float.MaxValue, ErrorMessage = "Favor ingresar un numero mayor que 0")]
+    [Required(ErrorMessage = "El campo SueldoHora es obligatorio")]
+    public decimal SueldoHora { get; set; }
+    
+}
